0f59023a33c62ab34b108f3a066fbec235e9b0f3
tim.pogue <tim.pogue@cybera.net>
2020-06-05 16:31:14 -0500

    constant variables

---
+++ constants/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const int NUM = 5;          // constant variables are normally done with all cap letters in name.
+    printf("%d\n", NUM);
+
+    // num = 8;
+    printf("%d\n", NUM);
+
+    return 0;
+}
