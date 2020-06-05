07bc6037d95dcd84102ed5c5638568020f671475
tim.pogue <tim.pogue@cybera.net>
2020-06-05 16:11:24 -0500

    datatypes notes for video

---
+++ datatypes/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 40;           //Whole number or counting number like (1, 2, 3).
+    double gpa = 3.7;       //Decimal number like (2.5, 4.5).
+    float gpa_2_0 = 3.2;    //double's value is large than float, and also takes up more memory.
+    char grade = 'A';       //A single character.  Only able to store one character.
+    char phase[] = "Words"; //This is a string of characters.  technically an array of characters.  Unmutable... cannot be changed.
+
+    return 0;
+}
