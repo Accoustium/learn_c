514a2d43ee984277983915c887ad9c2c428dd256
tim.pogue <tim.pogue@cybera.net>
2020-06-05 16:18:13 -0500

    added note for character format specifier

--- printf/main.c
+++ printf/main.c
@@ -5,9 +5,10 @@ int main()
 {
     printf("Hello\nWorld");         // \n is a new-line character.
     printf("Hello\"World");         // \ is used to escape characters to print the character.
-    print("%d", 4);                 // %d is called a format specifier, for integers.
-    print("%s", "Hello");           // %s is for strings.
-    pring("%f", 3.4);               // %f is used for float or double numbers.
+    printf("%d", 4);                // %d is called a format specifier, for integers.
+    printf("%s", "Hello");          // %s is for strings.
+    printf("%f", 3.4);              // %f is used for float or double numbers.
+    print("%c", 'i');               // %c is for single characters.  Cannot be used on strings.
 
     return 0;
 }
