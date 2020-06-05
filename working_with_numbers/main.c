a70d6ea8a37735919ee564e786d50e6c3d05ce46
tim.pogue <tim.pogue@cybera.net>
2020-06-05 16:26:29 -0500

    fun with numbers

---
+++ working_with_numbers/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int num = 6;
+    printf("%f\n", 5 / 4.5);
+    printf("%d\n", num);
+    printf("%f\n", pow(2, 3));
+    printf("%f\n", sqrt(36));
+
+    return 0;
+}
